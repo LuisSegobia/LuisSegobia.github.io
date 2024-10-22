625debb7ee6ea67b89549a604e8b193ba75f32f9
LuisSegobia <luisjose200485@gmail.com>
2024-10-22 10:49:51 -0500

    reestructuracion

---
+++ script.js
@@ -0,0 +1,4 @@
+var mensaje = prompt("te cepillaste los dientes hoy? \n Solo responde con 'si o no' ");
+var verdadero = ("si");
+if (mensaje == verdadero){alert("Te felicito!!!")}
+else {alert("COCHINOOO")}
