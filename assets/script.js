2034e7efdf9736d24a1dcc4aeb479e772d5530ea
LuisSegobia <luisjose200485@gmail.com>
2024-10-28 22:36:52 -0500

    mas funciones

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,36 @@
-var mensaje = prompt("te cepillaste los dientes hoy? \n Solo responde con 'si o no' ");
-var verdadero = ("si");
-if (mensaje == verdadero){alert("Te felicito!!!")}
-else {alert("COCHINOOO")}
+var numero = (prompt("ingresa un numero \n y te dire si es par o impar"))
+var resultado =parImpar(numero)
+
+alert("el numero "+numero+" es "+resultado)
+function parImpar(numero){
+
+    if (numero % 2 ==0){
+        return "par";}
+
+    else {
+    return "impar";} 
+}
+
+
+
+var cadena = (prompt("introduzca un texto, \n veremos si tiene mayusculas, \n minusculas o ambas"))
+var resultado =info(cadena)
+alert (resultado)
+
+
+function info(cadena){
+    if(cadena == cadena.toUpperCase()){
+
+        return("cadena de texto mayuscula");
+
+    }
+    else if (cadena==cadena.toLowerCase()){
+        return("cadena de texto minusculas");
+    }
+    else {
+        return"cadena de texto combinada";
+    }
+
+    
+
+}
